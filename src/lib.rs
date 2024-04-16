e32db26e97897cad07f8567b32655e1931b77a83
honehone12 <marshalc@hotmail.co.jp>
2024-04-16 15:54:58 +0900

    added buffers for server

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@ use std::fmt::Debug;
 use std::io::Cursor;
 
 use bevy::prelude::*;
+use bevy::utils::HashMap;
 use bevy_replicon::bincode;
 use bevy_replicon::bincode::deserialize_from;
 use bevy_replicon::client::client_mapper::ServerEntityMap;
-use bevy_replicon::client::ServerEntityTicks;
 use bevy_replicon::core::replication_fns::{
     self, ComponentFns, DeserializeFn, RemoveFn, SerializeFn
 };
@@ -20,9 +20,13 @@ use serde::{Deserialize, Serialize};
 
 pub use bevy_replicon_snap_macros;
 
-pub struct SnapshotInterpolationPlugin {
+pub struct RepliconSnapPlugin;
+
+#[derive(Resource, Serialize, Deserialize, Debug)]
+pub struct RepliconSnapConfig {
     /// Should reflect the server max tick rate
     pub max_tick_rate: u16,
+    pub max_buffer_size: usize
 }
 
 /// Sets for interpolation systems.
@@ -38,13 +42,12 @@ pub enum InterpolationSet {
     Interpolate,
 }
 
-impl Plugin for SnapshotInterpolationPlugin {
+impl Plugin for RepliconSnapPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<Interpolated>()
             .register_type::<OwnerPredicted>()
             .register_type::<NetworkOwner>()
             .register_type::<Predicted>()
-            .replicate::<Interpolated>()
             .replicate::<NetworkOwner>()
             .replicate::<OwnerPredicted>()
             .configure_sets(PreUpdate, InterpolationSet::Init.after(ClientSet::Receive))
@@ -57,16 +60,8 @@ impl Plugin for SnapshotInterpolationPlugin {
                 owner_prediction_init_system
                     .run_if(resource_exists::<NetcodeClientTransport>)
                     .in_set(InterpolationSet::Init),
-            )
-            .insert_resource(InterpolationConfig {
-                max_tick_rate: self.max_tick_rate,
-            });
+            );
     }
-}
-
-#[derive(Resource, Serialize, Deserialize, Debug)]
-pub(crate) struct InterpolationConfig {
-    max_tick_rate: u16,
 }
 
 #[derive(Component, Deserialize, Serialize, Reflect)]
@@ -98,12 +93,12 @@ pub trait Interpolate {
 }
 
 #[derive(Deserialize, Serialize, Reflect)]
-pub struct Snapshot<T: Component + Interpolate + Clone> {
+pub struct ComponentSnapshot<T: Component + Interpolate + Clone> {
     tick: u32,
     value: T,
 }
 
-impl<T: Component + Interpolate + Clone> Snapshot<T> {
+impl<T: Component + Interpolate + Clone> ComponentSnapshot<T> {
     #[inline]
     pub fn new(value: T, tick: u32) -> Self {
         Self { 
@@ -124,37 +119,57 @@ impl<T: Component + Interpolate + Clone> Snapshot<T> {
 }
 
 #[derive(Component, Deserialize, Serialize, Reflect)]
-pub struct SnapshotBuffer<T: Component + Interpolate + Clone> {
-    buffer: VecDeque<Snapshot<T>>,
+pub struct ComponentSnapshotBuffer<T: Component + Interpolate + Clone> {
+    buffer: VecDeque<ComponentSnapshot<T>>,
     time_since_last_snapshot: f32,
     latest_snapshot_tick: u32,
+    max_buffer_size: usize
 }
 
-impl<T: Component + Interpolate + Clone> SnapshotBuffer<T> {
-    pub fn new() -> Self {
+impl<T: Component + Interpolate + Clone> ComponentSnapshotBuffer<T> {
+    #[inline]
+    pub fn new(max_buffer_size: usize) -> Self {
         Self {
             buffer: VecDeque::new(),
             time_since_last_snapshot: 0.0,
             latest_snapshot_tick: 0,
+            max_buffer_size
         }
     }
+
+    #[inline]
     pub fn insert(&mut self, element: T, tick: u32) {
-        if self.buffer.len() > 1 {
+        if self.buffer.len() >= self.max_buffer_size {
             self.buffer.pop_front();
         }
-        self.buffer.push_back(Snapshot::new(element, tick));
+
+        // !!
+        // check tick
+        // because transport might be unreliable
+
+        self.buffer.push_back(ComponentSnapshot::new(element, tick));
         self.time_since_last_snapshot = 0.0;
         self.latest_snapshot_tick = tick;
     }
 
     #[inline]
-    pub fn latest_snapshot(&self) -> Option<&Snapshot<T>> {
+    pub fn latest_snapshot(&self) -> Option<&ComponentSnapshot<T>> {
         self.buffer.back()
     }
 
     #[inline]
     pub fn latest_snapshot_tick(&self) -> u32 {
         self.latest_snapshot_tick
+    }
+
+    #[inline]
+    pub fn at(&self, at: usize) -> Option<&ComponentSnapshot<T>> {
+        self.buffer.get(at)
+    }
+
+    #[inline]
+    pub fn iter(&self) -> Iter<'_, ComponentSnapshot<T>> {
+        self.buffer.iter()
     }
 
     #[inline]
@@ -195,37 +210,103 @@ impl<T: Event> EventSnapshot<T> {
     } 
 }
 
+pub struct EventSnapshotHistory<T: Event> {
+    buffer: VecDeque<EventSnapshot<T>>,
+    latest_snapshot_tick: u32
+}
+
+impl<T: Event> EventSnapshotHistory<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self { 
+            buffer: VecDeque::new(), 
+            latest_snapshot_tick: 0 
+        }
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    #[inline]
+    pub fn latest_snapshot_tick(&self) -> u32 {
+        self.latest_snapshot_tick
+    }
+
+    #[inline]
+    pub fn iter(&self) -> Iter<'_, EventSnapshot<T>> {
+        self.buffer.iter()
+    }
+} 
+
 #[derive(Resource)]
-pub struct PredictedEventHistory<T: Event>(pub VecDeque<EventSnapshot<T>>);
+pub struct PredictedEventHistory<T: Event> {
+    buffer: HashMap<u64, EventSnapshotHistory<T>>,
+    max_buffer_size: usize
+}
 
 impl<T: Event> PredictedEventHistory<T> {
-    pub fn new() -> PredictedEventHistory<T> {
-        Self(VecDeque::new())
+    #[inline]
+    pub fn new(max_buffer_size: usize) -> PredictedEventHistory<T> {
+        Self{
+            buffer: default(),
+            max_buffer_size
+        }
     }
-    pub fn insert(&mut self, value: T, tick: u32, delta_time: f32) -> &mut Self {
-        self.0.push_back(EventSnapshot {
+
+    #[inline]
+    pub fn clients_count(&self) -> usize {
+        self.buffer.len()
+    }
+
+    #[inline]
+    pub fn len(&self, client_id: u64) -> usize {
+        match self.buffer.get(&client_id) {
+            Some(h) => h.len(),
+            None => 0
+        }
+    }
+
+    #[inline] 
+    pub fn history(&self, client_id: u64) -> Option<&EventSnapshotHistory<T>> {
+        self.buffer.get(&client_id)
+    }
+
+    pub fn insert(&mut self, client_id: u64, value: T, tick: u32, delta_time: f32) {
+        let history = self.buffer
+        .entry(client_id)
+        .or_insert(EventSnapshotHistory::new());
+
+        // !!
+        // check tick
+        // because transport might be unreliable
+
+        if history.buffer.len() >= self.max_buffer_size {
+            history.buffer.pop_front();
+        }
+
+        history.buffer.push_back(EventSnapshot {
             value,
             tick,
             delta_time,
         });
-        self
-    }
-    pub fn remove_stale(&mut self, latest_server_snapshot_tick: u32) -> &mut Self {
-        if let Some(last_index) = self
-            .0
-            .iter()
-            .position(|v| v.tick >= latest_server_snapshot_tick)
-        {
-            self.0.drain(0..last_index);
-        } else {
-            self.0.clear();
-        }
-        self
+        history.latest_snapshot_tick = tick;
     }
 
-    pub fn predict(&mut self, latest_server_snapshot_tick: u32) -> Iter<'_, EventSnapshot<T>> {
-        self.remove_stale(latest_server_snapshot_tick);
-        self.0.iter()
+    pub fn frontier(&mut self, client_id: u64, frontier_tick: u32) 
+    -> Iter<'_, EventSnapshot<T>> {
+        let history = self.buffer
+        .entry(client_id)
+        .or_insert(EventSnapshotHistory::new());
+
+        if let Some(last_index) = history.buffer
+        .iter()
+        .position(|v| v.tick >= frontier_tick) {
+            history.buffer.range(last_index..)
+        } else {
+            history.buffer.range(0..0)
+        }
     }
 }
 
@@ -241,52 +322,56 @@ fn owner_prediction_init_system(
         } else {
             commands.entity(e).insert(Interpolated);
         }
-    }
+    }    
 }
 
 /// Initialize snapshot buffers for new entities.
-fn snapshot_buffer_init_system<T: Component + Interpolate + Clone>(
-    q_new: Query<(Entity, &T), Or<(Added<Predicted>, Added<Interpolated>)>>,
+fn component_snapshot_buffer_init_system<T: Component + Interpolate + Clone>(
     mut commands: Commands,
-    server_ticks: Res<ServerEntityTicks>,
+    q_new: Query<Entity, (With<T>, Added<OwnerPredicted>)>,
+    snap_config: Res<RepliconSnapConfig>
 ) {
-    for (e, initial_value) in q_new.iter() {
-        if let Some(tick) = (*server_ticks).get(&e) {
-            let mut buffer = SnapshotBuffer::new();
-            buffer.insert(initial_value.clone(), tick.get());
-            commands.entity(e).insert(buffer);
-        }
+    // !!
+    // no need to cache all components
+    // want config for each
+
+    for e in q_new.iter() {
+        let buffer = ComponentSnapshotBuffer::<T>::new(snap_config.max_buffer_size);
+        commands.entity(e).insert(buffer).log_components();
     }
 }
 
 /// Interpolate between snapshots.
 fn snapshot_interpolation_system<T: Component + Interpolate + Clone>(
-    mut q: Query<(&mut T, &mut SnapshotBuffer<T>), (With<Interpolated>, Without<Predicted>)>,
+    mut q: Query<(&mut T, &mut ComponentSnapshotBuffer<T>), (With<Interpolated>, Without<Predicted>)>,
     time: Res<Time>,
-    config: Res<InterpolationConfig>,
+    snap_config: Res<RepliconSnapConfig>,
 ) {
     for (mut component, mut snapshot_buffer) in q.iter_mut() {
-        let buffer = &snapshot_buffer.buffer;
-        let elapsed = snapshot_buffer.time_since_last_snapshot;
-        if buffer.len() < 2 {
+        let buff_len =  snapshot_buffer.buffer.len();
+        if buff_len < 2 {
             continue;
         }
 
-        let tick_duration = 1.0 / (config.max_tick_rate as f32);
-
+        let elapsed = snapshot_buffer.time_since_last_snapshot;
+        let tick_duration = 1.0 / (snap_config.max_tick_rate as f32);
         if elapsed > tick_duration + time.delta_seconds() {
             continue;
         }
 
         let t = (elapsed / tick_duration).clamp(0., 1.);
-        *component = buffer[0].value.interpolate(buffer[1].value.clone(), t);
+        let mut iter = snapshot_buffer.iter().rev();
+        let latest = iter.next().unwrap(); //buffer is longer than 2
+        let second = iter.next().unwrap();
+
+        *component = second.value.interpolate(latest.value.clone(), t);
         snapshot_buffer.time_since_last_snapshot += time.delta_seconds();
     }
 }
 
 /// Advances the snapshot buffer time for predicted entities.
 fn predicted_snapshot_system<T: Component + Interpolate + Clone>(
-    mut q: Query<&mut SnapshotBuffer<T>, (Without<Interpolated>, With<Predicted>)>,
+    mut q: Query<&mut ComponentSnapshotBuffer<T>, (Without<Interpolated>, With<Predicted>)>,
     time: Res<Time>,
 ) {
     for mut snapshot_buffer in q.iter_mut() {
@@ -301,7 +386,7 @@ pub fn deserialize_snap_component<C: Clone + Interpolate + Component + Deseriali
     tick: RepliconTick,
 ) -> bincode::Result<()> {
     let component: C = deserialize_from(cursor)?;
-    if let Some(mut buffer) = entity.get_mut::<SnapshotBuffer<C>>() {
+    if let Some(mut buffer) = entity.get_mut::<ComponentSnapshotBuffer<C>>() {
         buffer.insert(component, tick.get());
     } else {
         entity.insert(component);
@@ -310,7 +395,7 @@ pub fn deserialize_snap_component<C: Clone + Interpolate + Component + Deseriali
     Ok(())
 }
 
-pub trait AppInterpolationExt {
+pub trait RepliconSnapExt {
     /// TODO: Add docs
     fn replicate_interpolated<C>(&mut self) -> &mut Self
     where
@@ -331,7 +416,7 @@ pub trait AppInterpolationExt {
         C: Event + Serialize + DeserializeOwned + Debug + Clone;
 }
 
-impl AppInterpolationExt for App {
+impl RepliconSnapExt for App {
     fn replicate_interpolated<C>(&mut self) -> &mut Self
     where
         C: Component + Interpolate + Clone + Serialize + DeserializeOwned,
@@ -354,9 +439,8 @@ impl AppInterpolationExt for App {
     {
         self.add_systems(
             PreUpdate,
-            (snapshot_buffer_init_system::<T>.after(owner_prediction_init_system))
+            (component_snapshot_buffer_init_system::<T>.after(owner_prediction_init_system))
                 .in_set(InterpolationSet::Init)
-                .run_if(resource_exists::<RenetClient>),
         )
         .add_systems(
             PreUpdate,
@@ -381,7 +465,12 @@ impl AppInterpolationExt for App {
     where
         C: Event + Serialize + DeserializeOwned + Debug + Clone,
     {
-        let history: PredictedEventHistory<C> = PredictedEventHistory::new();
+        // !!
+        // no need to cache all events
+        // want config for each
+
+        let config = self.world.resource::<RepliconSnapConfig>();
+        let history = PredictedEventHistory::<C>::new(config.max_buffer_size);
         self.insert_resource(history);
         self.add_client_event::<C>(channel)
     }
